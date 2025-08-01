3cf63a47a636ed1732457c33af00f4bc4ebf0c14
Sidharth Malpani <135989098+areysid@users.noreply.github.com>
2025-08-01 09:41:45 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,28 +1,20 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Downgraded to avoid unsupported blocks
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = local.region
+  region = "us-east-1"
 }
 
 locals {
-  region = "us-east-1"
   name   = "sids-cluster-v1"
-
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-
-  tags = {
-    Example = local.name
-  }
+  region = "us-east-1"
+  azs    = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
