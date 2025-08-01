cffd16ef2aa11b20537ec68347c03b2c606c6298
Sidharth Malpani <135989098+areysid@users.noreply.github.com>
2025-08-01 09:42:14 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,24 +1,21 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"  # ✅ updated from 4.0
+  version = "4.0.2"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "${local.name}-vpc"
+  cidr = "10.0.0.0/16"
 
   azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+  tags = {
+    Name        = "${local.name}-vpc"
+    Environment = "dev"
   }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-  tags = local.tags
 }
